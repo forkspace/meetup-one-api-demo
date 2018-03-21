5642dd0b6e81e41412278a0b104182803adee1ed
Unknown <chrispaton2005@gmail.com>
2018-03-21 20:37:10 +0000

    Added dynamodb schema. Basic HTTP configuration.

--- README.md
+++
@@ -1,1 +0,0 @@
-sls-meetup-demo
--- meetups.js
+++ meetups.js
@@ -1,16 +1,16 @@
-'use strict';
+const { Meetups } = require('./lib/models');
 
 module.exports.getMeetups = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  try {
+    Meetups.scan()
+      .loadAll()
+      .exec(callback);
+  } catch (error) {
+    callback(error);
+  }
+};
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.createMeetup = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  Meetups.create(body.meetup, callback);
 };
